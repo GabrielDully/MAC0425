6fa6a51f08e863e9f3cc3daaf5889a9221afb46b
GabrielDully <gabrieldullymantovani@gmail.com>
2025-04-19 14:03:48 -0300

    feat(prolog): cap1/ex2.pl

--- prolog/cap1/ex3.pl
+++ prolog/cap1/ex3.pl
@@ -1,1 +1,16 @@
+pai(naruto,boruto).
+pai(naruto,himawari).
+pai(sasuke,sarada).
+mae(hinata,boruto).
+mae(hinata,himawari).
+mae(sakura,sarada).
 
+% a) Todo mundo que tem filho é feliz.
+tem_filho(X) :- pai(X,Y).
+tem_filho(X) :- mae(X,Y).
+
+feliz(X) :- tem_filho(X).
+
+% b) Um casal é formado por duas pessoas que têm filhos em comum (definição).
+casal(X,Y) :- pai(X,Z), mae(Y,Z).
+casal(X,Y) :- pai(Y,Z), mae(X,Z).
