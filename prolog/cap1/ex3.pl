5937ea7139e9172830d110531562993383b7ea0d
GabrielDully <gabrieldullymantovani@gmail.com>
2025-04-19 15:50:36 -0300

    chore(prolog): cap1/ex3

--- prolog/cap1/ex3.pl
+++ prolog/cap1/ex3.pl
@@ -11,6 +11,13 @@ tem_filho(X) :- mae(X,Y).
 
 feliz(X) :- tem_filho(X).
 
+% feliz(X) :- tem_filho(X, _).
+
 % b) Um casal é formado por duas pessoas que têm filhos em comum (definição).
 casal(X,Y) :- pai(X,Z), mae(Y,Z).
 casal(X,Y) :- pai(Y,Z), mae(X,Z).
+
+% casal(X,Y) :-
+%     tem_filho(X,F),
+%     tem_filho(Y,F),
+%     X \= Y.
