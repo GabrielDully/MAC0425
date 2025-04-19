6a0ad2582b6f5e89bc65567b61b283ed0a37c522
GabrielDully <gabrieldullymantovani@gmail.com>
2025-04-19 16:59:03 -0300

    feat(prolog): cap3/ex2

--- prolog/cap3/ex2.pl
+++ prolog/cap3/ex2.pl
@@ -1,1 +1,6 @@
-
+% 3.2. O predicado `num` classifica números em três categorias: 'positivos', 'nulo' e 'negativos'.
+% Esse predicado, da maneira como está definido, realiza retrocesso desnecessário.
+% Explique por que isso acontece e, em seguida, aplique cortes para eliminar esse retrocesso.
+num(N,positivo) :- N > 0, !.
+num(N,negativo) :- N < 0, !.
+num(N,nulo).
